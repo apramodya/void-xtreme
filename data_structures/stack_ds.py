3f0e17a8227f95304e75b305027538043fca83c6
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-10-28 23:29:45 +0530

    Add comments for stack ds

--- data_structures/stack_ds.py
+++ data_structures/stack_ds.py
@@ -1,16 +1,20 @@
 class Stack:
+    # Initialization
     def __init__(self):
         self.items = list()
 
+    # Push item
     def push(self, item):
         self.items.append(item)
 
+    # Pop item
     def pop(self):
         if len(self.items) > 0:
             return self.items.pop()
         else:
             return None
 
+    # Get item from the frond end
     def peek(self):
         if len(self.items) > 0:
             item = self.items[-1]
@@ -18,8 +22,10 @@ class Stack:
         else:
             return None
 
+    # Get the size of the stack
     def size(self):
         return len(self.items)
 
+    # Check whether the stack is empty or not
     def is_empty(self):
         return len(self.items) == 0
